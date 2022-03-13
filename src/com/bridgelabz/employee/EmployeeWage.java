a0053bb14936afef6b663ca8b081b97db2471227
Aditya Bahakar <adityabahakar99@gmail.com>
2022-03-14 05:26:21 +0530

    Calculating wage till a condition

--- src/com/bridgelabz/employee/EmployeeWage.java
+++ src/com/bridgelabz/employee/EmployeeWage.java
@@ -2,13 +2,16 @@
 package com.bridgelabz.employee;
 
 public class EmployeeWage {
-    public static void main(String[] args) {
+    public static void main(String[] Args) {
         int IsFullTime = 1;
+        int IsPartTime = 2;
         int EmployeRatePerHour = 20;
         int EmployeWages = 0;
         int EmployeHour = 0;
-        double EmpCheck = Math.floor(Math.random() * 10) % 2;
+        double EmpCheck = Math.floor(Math.random() * 10) % 3;
         if (EmpCheck == IsFullTime)
+            EmployeHour = 8;
+        else if (EmpCheck == IsPartTime)
             EmployeHour = 8;
         else
             EmployeHour = 0;
